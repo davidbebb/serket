d327e1f60e393e745e1899724c52af31dd056873
David Bebb <davidbebb@gmail.com>
2015-12-22 09:11:52 +0000

    date filtering now with moment range

--- google_charts_js.js
+++ google_charts_js.js
@@ -29,23 +29,37 @@ var connectedLink2 = "http://serket.uk/badges/badgelist"
 $.getJSON(connectedLink2, function showData(data){
 
   data = filterByDate(data);
+  var start  = new Date(2015, 10, 0);
+  var end    = new Date(2015, 10, 30);
+  var range = moment.range(start , end);
 
-  data = data.filter(datum => datum.dispenseDate.getMonth()===10)
-  // throw out everything with an illegitimate medicine name
-  var dataHappyName = data.filter(datum => happyMedName(datum.medicineName));
-  // extract the medicine names that make sense
-  var happyNameList = unique(dataHappyName.map(datum => getMedName(datum.medicineName))).sort();
-  // count up the number of times each medicine is prescribed
-  var happyNameScore = happyNameList.map(name => howManyTimes(name, dataHappyName));
+  data = data.filter(datum => range.contains(datum.dispenseDate));
 
-  // zip up the two arrays, sort them, unzips again
-  var happyNameBoth = happyNameList.map((name, index) => [name, happyNameScore[index]]);
+  //data = data.filter(datum => datum.dispenseDate.getMonth()===10);
+  dataTrue = data.filter(datum => datum.statePattern);
+  dataFalse = data.filter(datum => !datum.statePattern);
+
+  scoreAccuracy = function(data){
+    dispenseDates = unique(data.map(datum => datum.dispenseDate))
+
+  };
 
   var dispenseDates = unique(data.map(datum => datum.dispenseDate))
 
   var dateScore = dispenseDates.map(name => howManyTimesDate(name.getDate(), data))
 
   var salesTime = dispenseDates.map((datum, index) => [datum, dateScore[index]]);
-  console.log(salesTime)
   drawMedsPieChart(salesTime)
+
 });
+
+
+// // throw out everything with an illegitimate medicine name
+// var dataHappyName = data.filter(datum => happyMedName(datum.medicineName));
+// // extract the medicine names that make sense
+// var happyNameList = unique(dataHappyName.map(datum => getMedName(datum.medicineName))).sort();
+// // count up the number of times each medicine is prescribed
+// var happyNameScore = happyNameList.map(name => howManyTimes(name, dataHappyName));
+//
+// // zip up the two arrays, sort them, unzips again
+// var happyNameBoth = happyNameList.map((name, index) => [name, happyNameScore[index]]);
